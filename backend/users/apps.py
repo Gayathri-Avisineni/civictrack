954b38e6b7ea8675e547defa1cd08483834a9795
Gayathri <gayathri.avisineni@gmail.com>
2026-05-24 15:52:16 +0530

    Connect PostgreSQL database

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -4,3 +4,18 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def create_admin():
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@test.com",
+            password="admin123"
+        )
+
+create_admin()
